9cd0e8144bd1bd883932f434c87fd2d9925dd029
danvisai <danvisai03@gmail.com>
2022-03-28 12:27:13 +0530

    market

--- components/Market.js
+++ components/Market.js
@@ -1,105 +1,47 @@
 import Head from 'next/head'
 import { useState } from 'react';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import { ethers } from 'ethers';
 import { useWeb3 } from '@3rdweb/hooks';
 import Hero from "../components/Hero"
 import Header from '../components/Header';
 import toast, { Toaster } from 'react-hot-toast';
 
-import Web3Modal from 'web3Modal;'
-const style = {
-    wrapper: ``,
-    NFTcard: `justify justifu top left list `,
-    button: `border border-[#282b2f] bg-[orange] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
-    details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
-    background: `max-w-sm rounded overflow-hidden shadow-lg bg-[white]`,
-    cardsize: `px-6 py-4`,
-    bigFont: `font-bold text-xl mb-2`,
-    smallFont: `text-gray-700 text-base`,
-    bigButton: `bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded`
+import web3Modal from 'web3Modal'
 
-}
+import Link from 'next/link';
 
-
-
-async function loadNFTs() {
-    /* create a generic provider and query for unsold market items */
-    const provider = new ethers.providers.JsonRpcProvider()
-    //const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, provider)
-    const data = await contract.fetchMarketItems()
-
-    /*
-    *  map over items returned from smart contract and format 
-    *  them as well as fetch their token metadata
-    */
-    const items = await Promise.all(data.map(async i => {
-      const tokenUri = await contract.tokenURI(i.tokenId)
-      const meta = await axios.get(tokenUri)
-      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
-      let item = {
-        price,
-        tokenId: i.tokenId.toNumber(),
-        seller: i.seller,
-        owner: i.owner,
-        image: meta.data.image,
-        name: meta.data.name,
-        description: meta.data.description,
-      }
-      return item
-    }))
-    setNfts(items)
-    setLoadingState('loaded') 
-  }
-  async function buyNft(nft) {
-    /* needs the user to sign the transaction, so will use Web3Provider and sign it */
-    const web3Modal = new Web3Modal()
-    const connection = await web3Modal.connect()
-    const provider = new ethers.providers.Web3Provider(connection)
-    const signer = provider.getSigner()
-    const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
-
-    /* user will be prompted to pay the asking proces to complete the transaction */
-    const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')   
-    const transaction = await contract.createMarketSale(nft.tokenId, {
-      value: price
-    })
-    await transaction.wait()
-    loadNFTs()
-  }
-  
-
-export default function market() {
-    const [nfts, setNfts] = useState([])
-  const [loadingState, setLoadingState] = useState('not-loaded')
-  useEffect(() => {
-    loadNFTs()
-  }, [])
-
-    return (
-        <div className="flex justify-center">
-      <div className="px-4" style={{ maxWidth: '1600px' }}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
-          {
-            nfts.map((nft, i) => (
-              <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} />
-                <div className="p-4">
-                  <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
-                  <div style={{ height: '70px', overflow: 'hidden' }}>
-                    <p className="text-gray-400">{nft.description}</p>
-                  </div>
-                </div>
-                <div className="p-4 bg-black">
-                  <p className="text-2xl font-bold text-white">{nft.price} ETH</p>
-                  <button className="mt-4 w-full bg-pink-500 text-white font-bold py-2 px-12 rounded" onClick={() => buyNft(nft)}>Buy</button>
-                </div>
-              </div>
-            ))
-          }
+function Market() {
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">AgriCrop Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-pink-500">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-nft">
+            <a className="mr-6 text-pink-500">
+              Sell NFT
+            </a>
+          </Link>
+          <Link href="/my-nfts">
+            <a className="mr-6 text-pink-500">
+              My NFTs
+            </a>
+          </Link>
+          <Link href="/dashboard">
+            <a className="mr-6 text-pink-500">
+              Dashboard
+            </a>
+          </Link>
         </div>
-      </div>
-    </div>
+      </nav>
 
-    )
+    </div>
+  )
 }
+
+export default Market
