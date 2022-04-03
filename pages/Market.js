9356e1649114c59e7bca2107d5249e4769bfe06b
danvisai <danvisai03@gmail.com>
2022-04-03 12:51:51 +0530

    css marketplace

--- pages/Market.js
+++ pages/Market.js
@@ -11,11 +11,10 @@ import web3Modal from 'web3Modal'
 import Link from 'next/link';
 
 const style = {
-  wrapper: ``,
-  walletConnectWrapper: `bg-[lightskyblue] flex flex-col justify-center items-center h-screen w-screen  `,
+  wrapper: `relative`,
+  walletConnectWrapper: `before:content-['']  before:absolute before:top-0 before:left-0 before:right-0 before:bottom-0 before:bg-[url('https://www.linkpicture.com/q/backm.png')] before:bg-cover before:bg-center before:opacity-75 `,
   button: `border border-[#282b2f] bg-[orange] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
   details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
-  background: `max-w-sm rounded-lg overflow-hidden shadow-lg bg-[white]`,
   cardsize: `px-6 py-4`,
   bigFont: `text=[#D37506]-700   font-bold text-xl mb-2`,
   smallFont: `text-gray-700 text-base`,
@@ -25,7 +24,8 @@ const style = {
 function Market() {
   return (
     <div className={style.walletConnectWrapper}>
-      <div>
+
+      <div className={style.wrapper}>
         <nav className="border-b p-10">
           <p className="text-7xl font-medium">AgriCrop Marketplace</p>
           <div className="flex mt-4">
@@ -52,7 +52,9 @@ function Market() {
           </div>
         </nav>
       </div>
-      </div>
+    </div>
+
+
   )
 }
 export default Market
